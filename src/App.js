84f3cfd55c082f86f2dc48941a54fbacda82a8d5
Juan Maurente <juanmaurente@gmail.com>
2022-08-05 16:17:02 +1000

    Responsive Hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import styles from './App.module.scss';
 import Navbar from './containers/Navbar';
+import Hero from './containers/Hero/Hero';
 
 function App() {
 	return (
-		<div className='App'>
+		<div className={styles.App}>
 			<Navbar />
+			<Hero />
 		</div>
 	);
 }
---
+++ src/containers/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import styles from '../Hero/Hero.module.scss';
+import img from '../../assets/fabian-albert-iiPOd73iCUQ-unsplash.jpg';
+
+function Hero() {
+	return (
+		<div className={styles.fullContainer}>
+			<img
+				className={styles.hero_back}
+				src={img}
+				alt='phone background'
+			/>
+			<div className={styles.hero_block}>
+				<h2 className={styles.hero_title}>
+					Everything you can imagine is here
+				</h2>
+				<button className={styles.hero_button}>Shop Now</button>
+			</div>
+		</div>
+	);
+}
+
+export default Hero;
