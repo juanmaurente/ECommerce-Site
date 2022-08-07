068f8652f87baab7ce01b20b0b5ebe32a8ab7e70
Juan Maurente <juanmaurente@gmail.com>
2022-08-08 09:17:51 +1000

    gitignore

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -3,13 +3,17 @@ import { useEffect, useState } from 'react';
 import Link from '../../components/Link';
 import logo from '../../assets/icons/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
 	const [displayLinks, setDisplayLinks] = useState(true);
 
 	const handleDisplay = () => {
 		setDisplayLinks(!displayLinks);
+	};
+
+	const handleCart = () => {
+		console.log('hola');
 	};
 
 	return (
@@ -22,16 +26,23 @@ function Navbar() {
 						alt='tech logo'
 					/>
 				</div>
-				<div
-					className={
-						displayLinks
-							? styles.navbar_links
-							: styles.navbar_links_hidden
-					}>
-					<Link title={'Link 1'} />
-					<Link title={'Link 2'} />
-					<Link title={'Link 3'} />
-					<Link title={'Link 4'} />
+				<div className={styles.leftBlock}>
+					<div
+						className={
+							displayLinks
+								? styles.navbar_links
+								: styles.navbar_links_hidden
+						}>
+						<Link title={'Link 1'} />
+						<Link title={'Link 2'} />
+						<Link title={'Link 3'} />
+						<Link title={'Link 4'} />
+					</div>
+					<FontAwesomeIcon
+						className={styles.navbar_cart}
+						icon={faShoppingCart}
+						onClick={handleCart}
+					/>
 				</div>
 				<FontAwesomeIcon
 					className={styles.navbar_bars}
---
+++ src/containers/ProductPage/ProductPage.jsx
@@ -0,0 +1,7 @@
+import styles from '../ProductPage/ProductPage.module.scss';
+
+function ProductPage() {
+	return <div>ProductPage</div>;
+}
+
+export default ProductPage;
